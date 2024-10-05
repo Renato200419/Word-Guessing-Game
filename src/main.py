eeba622cb2ce7515accbdc0d31c142acd2ac4c62
Renato200419 <renato.olivera.c@uni.pe>
2024-10-05 12:31:48 -0500

    Mostrar salida

--- src/main.py
+++ src/main.py
@@ -1,2 +1,6 @@
-print("Bienvenido al Juego de Adivinanza de Palabras!")
-print("La configuración inicial del juego se ha completado.")
+# print("Bienvenido al Juego de Adivinanza de Palabras!")
+# print("La configuración inicial del juego se ha completado.")
+
+from word_selector import seleccionar_palabra
+
+seleccionar_palabra()
