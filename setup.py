edf3aaf77427fa4ebec81314b0cd8d0edb49c793
Renato200419 <renato.olivera.c@uni.pe>
2024-10-06 10:46:00 -0500

    Configurar archivo

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='word_guessing_game',
-    version='3.0.0',
-    packages=find_packages(),
+    version='1.0.0',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     entry_points={
         'console_scripts': [
             'word-guessing-game = src.main:main',
@@ -12,10 +13,5 @@ setup(
     description='Juego de adivinanza de palabras desarrollado en Python.',
     author='Renato Olivera',
     author_email='renato.olivera.c@uni.pe',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',
+    install_requires=[],
 )
